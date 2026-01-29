47dab6937e59225efda2d992fabf857824e5a51b
ParkGyuweon <rbdnjs9@gmail.com>
2026-01-29 14:02:01 +0900

    끝

---
+++ day_05/main.py
@@ -0,0 +1,14 @@
+foo = 'asdf'
+bar = 'asdf'
+
+print(bar, end = ' ')
+print(len(bar))
+print(bar.strip(), end = ' ')
+print(len(bar.strip()))
+print(len(bar))
+
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+print(nums.append(5)) # None
+print(nums)
